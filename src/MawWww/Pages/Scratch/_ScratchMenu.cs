d81faa23d169a579559b3fcbdbc66bd03c637969
Mike Morano <mmorano@mikeandwan.us>
2024-10-20 11:40:28 -0400

    add the scratch apps

---
+++ src/MawWww/Pages/Scratch/_ScratchMenu.cs
@@ -0,0 +1,28 @@
+using MawWww.Models;
+
+namespace MawWww.Pages.Scratch;
+
+public static class ScratchMenu
+{
+    public static readonly SidebarMenu Menu = new (
+        [
+            new SidebarMenuItem("./DressUp",           "Dress Up"),
+            new SidebarMenuItem("./FishyFishy",        "Fishy Fishy Cross My Ocean"),
+            new SidebarMenuItem("./GoofyCarRace",      "Goofy Car Race"),
+            new SidebarMenuItem("./HideAndSeek",       "Hide and Seek"),
+            new SidebarMenuItem("./Hipnotize",         "Hipnotize"),
+            new SidebarMenuItem("./KittyKittyScratch", "Kitty Kitty Scratch"),
+            new SidebarMenuItem("./Maze",              "Maze"),
+            new SidebarMenuItem("./Me",                "Me"),
+            new SidebarMenuItem("./MooSic",            "Moo Sic"),
+            new SidebarMenuItem("./Pong",              "Pong"),
+            new SidebarMenuItem("./RaceCar2000",       "Race Car 2000"),
+            new SidebarMenuItem("./SuperMan",          "Super Man"),
+            new SidebarMenuItem("./Untitled",          "Untitled"),
+            new SidebarMenuItem("./WhosFaster",        "Who's Faster"),
+            new SidebarMenuItem("./WorkAndFade",       "Work and Fade"),
+            new SidebarMenuItem("./YummyYummyBaby",    "Yummy Yummy Baby")
+        ],
+        "Scratch Apps"
+    );
+}
---
+++ src/scratch/README.md
@@ -0,0 +1,16 @@
+# Scratch
+
+original scratch files - to prepare for web, use TurboWarp packager: https://packager.turbowarp.org/
+
+To package these scratch files for the web:
+
+1. clone packager project here: https://github.com/TurboWarp/packager
+2. pnpm ci
+3. pnpm start
+4. go to http://localhost:8947/
+
+Notes:
+
+- TurboMode does not seem to work for these projects
+- Adjust resolution as desired
+- Enable controls
