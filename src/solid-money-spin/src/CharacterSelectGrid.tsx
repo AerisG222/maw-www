d1a3c3f53cfc849496c2b6e3cb759745b29128c0
Mike Morano <mmorano@mikeandwan.us>
2024-12-09 22:03:56 -0500

    fix hover classes

--- src/solid-money-spin/src/CharacterSelectGrid.tsx
+++ src/solid-money-spin/src/CharacterSelectGrid.tsx
@@ -16,14 +16,14 @@ const CharacterSelectGrid: Component<Props> = (props) => {
 
             "maw-opacity-75": props.selectedCharacter?.name !== character.name,
             "maw-opacity-100": props.selectedCharacter?.name === character.name,
-            "maw-hover:opacity-100": true,
+            "hover:maw-opacity-100": true,
 
             "maw-border-solid": true,
             "maw-border-[4px]": true,
             "maw-rounded-xl": true,
             "maw-border-color-[transparent]": props.selectedCharacter?.name !== character.name,
             "maw-border-color-[red]": props.selectedCharacter?.name === character.name,
-            "maw-hover:border-color-[red]": true,
+            "hover:maw-border-color-[red]": true,
         };
 
         return classes;
@@ -33,7 +33,7 @@ const CharacterSelectGrid: Component<Props> = (props) => {
         <div class="maw-grid maw-grid-rows-[1fr_1fr] maw-grid-cols-[1fr_1fr] maw-gap-4">
         <For each={props.characters}>{ character =>
             <div classList={buildClasses(character)} onClick={() => props.selectCharacter(character.name)}>
-                <img class="maw-rounded-lg maw-h-auto maw-w-[200px]" src={character.imageUrl} />
+                <img class="maw-rounded-lg maw-h-auto maw-max-w-[200px]" src={character.imageUrl} />
             </div>
         }</For>
         </div>
