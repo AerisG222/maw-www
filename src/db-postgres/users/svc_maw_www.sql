123e95d2335b2e6446974c081042e6f1e1d8e17a
Mike Morano <mmorano@mikeandwan.us>
2024-12-28 23:29:01 -0500

    prefer create user over create role login

--- src/db-postgres/users/svc_maw_www.sql
+++ src/db-postgres/users/svc_maw_www.sql
@@ -5,7 +5,7 @@ BEGIN
                      FROM pg_catalog.pg_user
                     WHERE usename = 'svc_maw_www') THEN
 
-        CREATE ROLE svc_maw_www LOGIN INHERIT;
+        CREATE USER svc_maw_www;
         GRANT website TO svc_maw_www;
 
     END IF;
