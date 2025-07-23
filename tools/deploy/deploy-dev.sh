9223365fd6888fe11672efbcf9b2404a8d0fe995
Mike Morano <mmorano@mikeandwan.us>
2025-07-23 18:23:56 -0400

    -  limit var scope - start to create dirs / files at dest - start to build out podman quadlet for services

--- tools/deploy/deploy-dev.sh
+++ tools/deploy/deploy-dev.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+    #--connection-password-file ~/maw/dev/www/ansible/connection-password-file \
 ansible-playbook \
-    --connection-password-file ~/maw/dev/www/ansible/connection-password-file \
     --become-password-file ~/maw/dev/www/ansible/become-password-file \
     --inventory inventories/dev.yml \
     --extra-vars @vars.yml \
+    --extra-vars @~/maw/dev/www/ansible/secrets.yml \
     main.yml
